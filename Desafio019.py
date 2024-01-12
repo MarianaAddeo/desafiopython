d7aa50b6a8026969637344781d97d71b2def509f
MarianaAddeo <82729297+MarianaAddeo@users.noreply.github.com>
2024-01-12 04:28:00 -0300

    Add files via upload

---
+++ Desafio019.py
@@ -0,0 +1,8 @@
+import random
+n1 = str(input('Digite um nome: '))
+n2 = str(input('Digite um nome: '))
+n3 = str(input('Digite um nome: '))
+n4 = str(input('Digite um nome: '))
+lista = [n1, n2, n3, n4]
+escolhido = random.choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
---
+++ Desafio020.py
@@ -0,0 +1,9 @@
+import random
+n1 = str(input('Digite o nome do primeiro aluno: '))
+n2 = str(input('Digite o nome do segundo aluno: '))
+n3 = str(input('Digite o nome do terceiro nome: '))
+n4 = str(input('Digite o nome do quarto aluno: '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+print('A ordem da apresentação é: ')
+print(lista)
